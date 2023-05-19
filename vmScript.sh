5e4d18a5b24fc353a4f2bf8172f932a440086f47
Joshua Brookes <joshua_brookes3@hotmail.com>
2023-05-19 12:29:29 +1000

    Update vmScript.sh

--- vmScript.sh
+++ vmScript.sh
@@ -53,8 +53,7 @@ echo "CouchDB install, configure and setup"
 apt update && apt install -y curl apt-transport-https gnupg
 curl https://couchdb.apache.org/repo/keys.asc | gpg --dearmor | sudo tee /usr/share/keyrings/couchdb-archive-keyring.gpg >/dev/null 2>&1
 source /etc/os-release
-echo "deb [signed-by=/usr/share/keyrings/couchdb-archive-keyring.gpg] https://apache.jfrog.io/artifactory/couchdb-deb/ ${VERSION_CODENAME} main" \
-    | sudo tee /etc/apt/sources.list.d/couchdb.list >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/couchdb-archive-keyring.gpg] https://apache.jfrog.io/artifactory/couchdb-deb/ ${VERSION_CODENAME} main" | sudo tee /etc/apt/sources.list.d/couchdb.list >/dev/null
 
 apt update
 apt upgrade -y
