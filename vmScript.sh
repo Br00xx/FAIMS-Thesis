bf53d12be0272edc10209527494f0d74a6d41a22
Joshua Brookes <joshua_brookes3@hotmail.com>
2023-05-24 23:53:40 +1000

    Update vmScript.sh

--- vmScript.sh
+++ vmScript.sh
@@ -29,7 +29,8 @@ git clone https://github.com/FAIMS/FAIMS3-conductor.git
 
 cp FAIMS3-conductor/.env.dist FAIMS3-conductor/.env 
 echo "#########################################################"
-read -p ".env has been created. Please add your google client id and secret then press any key to continue..."
+echo ".env has been created. Please add your google client id and secret, and setup your couchdb username and password."
+read -p "Press any key to continue..."
 
 cd /opt/FAIMS3-conductor
 
