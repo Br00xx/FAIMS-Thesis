5f6000a7b4a0c6150bf6d3c604eb74a44c879b33
Joshua Brookes <joshua_brookes3@hotmail.com>
2023-05-30 21:08:59 +1000

    Update install-FAIMS3.sh

--- install-FAIMS3.sh
+++ install-FAIMS3.sh
@@ -1,9 +1,6 @@
+cd /opt
 git clone https://github.com/FAIMS/FAIMS3
-
 cp /opt/FAIMS-Thesis/FAIMS3.env /opt/FAIMS3/.env
-
 cd /opt/FAIMS3
-
 echo fs.inotify.max_user_watches= 100000 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-
 npm install
